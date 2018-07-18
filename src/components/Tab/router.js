6efa363efd269b8f65f04bd5ab6cbef5788d6f6d
mtoasis <mtoasis22@gmail.com>
2018-07-18 04:42:28 -0700

    features added

--- src/components/Tab/router.js
+++ src/components/Tab/router.js
@@ -18,8 +18,9 @@ const AvatarStack = createStackNavigator({
 export default Tabs = createBottomTabNavigator(
 
     {    
-        Avatar: { screen: AvatarStack }, 
         Schedule: { screen: ScheduleStack }, 
+        Avatar: { screen: AvatarStack }, 
+
 
     },
     {
--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View, Button, TouchableOpacity, ScrollView, Alert } f
 import { connect } from "react-redux";
 import store from '../../store'
 import axios from 'axios'
-import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
-
+import { SegmentedControls } from 'react-native-radio-buttons'
+import Modal from 'react-native-modal';
 
 let mapStateToProps = (store) => {
     return {
@@ -13,10 +13,9 @@ let mapStateToProps = (store) => {
     }
 }
 
-const radio_props = [
-    { label: 'rest', value: 'rest' },
-    { label: 'gym', value: 'gym' },
-    { label: 'study', value: 'study' }
+const options = [
+    "Academic",
+    "Atheletic"
 ];
 
 
@@ -29,6 +28,11 @@ class Schedule extends React.Component {
             schedule: [],
             clockCount: 18,
             isTimeArray: false,
+            isSent: false,
+            surveyQs: {},
+            surveyA1: {},
+            surveyA2: {},
+            visibleModal: null,
         }
     }
 
@@ -47,87 +51,127 @@ class Schedule extends React.Component {
         }
     }
 
+    fetchingSurvey() {
+        console.log("fetching survey")
+        axios.get("https://aatserver.herokuapp.com/api/survey")
+            .then(response => {
+                // console.log(response.data)
+                this.setState({ surveyQs: response.data[0] })
+                console.log(this.state.surveyQs)
+
+            })
+    }
+
     componentDidMount() {
 
         this.initializeTimeArray()
         this.props.navigation.setParams({
             sendPost: this.sendPost.bind(this)
         });
+        this.fetchingSurvey()
     }
 
     initializeTimeArray() {
-        console.log("running time array")
+
         for (var i = 6; i < 6 + this.state.clockCount; i++) {
             this.state.times.push(i)
         }
 
         for (var j = 0; j < this.state.clockCount; j++) {
-            this.state.schedule.push(-1)
+            this.state.schedule.push(null)
         }
         this.setState({
             isTimeArray: true,
         })
     }
 
+    callNextQ(choice) {
+        // this.state.surveyAs.q1.c = choice
+        console.log(choice)
+        // console.log(this.state.surveyAs)
+
+        return (
+            Alert.alert(`your choice:${choice}`)
+        )
+        // Alert.alert(
+
+        // )
+    }
+
     sendPost() {
+        if (this.props.isSignedIn) {
+            this.setState({ visibleModal: 3 })       
+        }
+        else {
+            Alert.alert("Please sign in")
+        }
+
+
+
+    }
+
+    postingData(){
+        console.log(this.state.surveyA1)
+        console.log(this.state.surveyA2)
 
         const schedule = this.state.schedule;
         const userInfo = this.props.userInfo;
 
-        for (var i = 0, n = schedule.length; i < n; i++) {
-            if (schedule[i] == -1) {
-                console.log(`running with index${i}`)
-                return (
-                    Alert.alert("Please finish your mark")
-                )
-            }
-        }
-        console.log(schedule)
-
-
         const info = {
             userID: userInfo.id,
             name: `${userInfo.name.givenName}, ${userInfo.name.familyName}`,
-            schedule: schedule
+            schedule: schedule,
+            surveyQA: {
+                q1:this.state.surveyA1.question,
+                c1:this.state.surveyA1.choice,
+                q2:this.state.surveyA2.question,
+                c2:this.state.surveyA2.choice,
+            }
         }
 
         axios.post("https://aatserver.herokuapp.com/api/posts", info)
             .then(response => {
-                console.log(response)
-                this.initializeTimeArray()
+                console.log(response.data)
+                // this.setState({ visibleModal: null }) //deleted due to unmounted component should not have setState
+                this.setState({ isSent: true })
+                console.log(`isSent:${this.state.isSent}`)
             })
 
         return (
             Alert.alert("Post Sent")
         )
-
     }
 
 
+
+
     printRow() {
-        console.log("running printRow")
-        console.log(this.state.schedule)
+
         return (
             <View>
 
                 {this.state.times.map((row, key) => {
+
                     return (
-                        <View key={key} style={{ flexDirection: "row", marginTop: 30, marginLeft: 10, marginTop: 30 }}>
-                            <Text>
-                                {row < 10 ? `0${row}:00 AM ~ 0${row}:00 AM` : `${row}:00 PM ~ ${row}:00 PM`}
+                        <View key={key} style={{ marginTop: 5 }}>
+                            <Text style={{ alignSelf: 'center', fontSize: 15 }}>
+                                {row == 11 ?
+
+                                    `${row}:00 AM ~ ${row + 1}:00 PM` :
+
+                                    row < 12 ?
+
+                                        `${row}:00 AM ~ ${row + 1}:00 AM` :
+
+                                        `${row}:00 PM ~ ${row + 1}:00 PM`}
+
                             </Text>
 
-                            <RadioForm
-                                radio_props={radio_props}
-                                initial={-1}
-                                formHorizontal={true}
-                                labelHorizontal={true}
-                                buttonColor={'#2196f3'}
-                                animation={true}
-                                onPress={(value) => {
-                                    this.state.schedule[key] = value
-                                    console.log(key)
-                                    console.log(this.state.schedule)
+                            <SegmentedControls
+                                options={options}
+                                onSelection={(selectedOption) => {
+                                    this.state.schedule[key] = selectedOption;
+                                    console.log(this.state.schedule);
                                 }}
                             />
                         </View>
@@ -139,30 +183,284 @@ class Schedule extends React.Component {
         )
     }
 
+    qOneModal() {
+
+        const surveyQs = this.state.surveyQs
+        return (
+            <Modal isVisible={this.state.visibleModal === 1}>
+
+                <View style={styles.modalContent}>
+                    <Text>{`Question1: ${surveyQs.q1.q}`}</Text>
+                    <TouchableOpacity onPress={() => {
+                        console.log(this.state)
+
+                        this.setState({
+                            visibleModal: 2,
+                            surveyA1: {
+                                question: surveyQs.q1.q,
+                                choice: surveyQs.q1.c1
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q1.c1}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 2,
+                            surveyA1: {
+                                question: surveyQs.q1.q,
+                                choice: surveyQs.q1.c2
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q1.c2}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 2,
+                            surveyA1: {
+                                question: surveyQs.q1.q,
+                                choice: surveyQs.q1.c3
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q1.c3}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 2,
+                            surveyA1: {
+                                question: surveyQs.q1.q,
+                                choice: surveyQs.q1.c4
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q1.c4}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 2,
+                            surveyA1: {
+                                question: surveyQs.q1.q,
+                                choice: surveyQs.q1.c5
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q1.c5}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => { this.setState({ visibleModal: null }) }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>cancel</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
+            </Modal>
+        )
+    }
+
+    qTwoModal() {
+
+        const surveyQs = this.state.surveyQs
+        return (
+            <Modal isVisible={this.state.visibleModal === 2}>
+                <View style={styles.modalContent}>
+                    <Text>{`Question2: ${surveyQs.q2.q}`}</Text>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 4,
+                            surveyA2: {
+                                question: surveyQs.q2.q,
+                                choice: surveyQs.q2.c1
+                            }                        
+                        })
+                    }}
+                    >
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q2.c1}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 4,
+                            surveyA2: {
+                                question: surveyQs.q2.q,
+                                choice: surveyQs.q2.c2
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q2.c2}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 4,
+                            surveyA2: {
+                                question: surveyQs.q2.q,
+                                choice: surveyQs.q2.c3
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q2.c3}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 4,
+                            surveyA2: {
+                                question: surveyQs.q2.q,
+                                choice: surveyQs.q2.c4
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q2.c4}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({
+                            visibleModal: 4,
+                            surveyA2: {
+                                question: surveyQs.q2.q,
+                                choice: surveyQs.q2.c5
+                            }
+                        })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>{surveyQs.q2.c5}</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({ visibleModal: null })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>cancel</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
+            </Modal>
+        )
+    }
+
+    sendingModal() {
+        return (
+            <Modal isVisible={this.state.visibleModal === 3}>
+                <View style={styles.modalContent}>
+                    <Text>Would you like to send?</Text>
+
+                    <TouchableOpacity onPress={()=>{
+                        this.setState({ visibleModal: 1 })
+                    }}
+                    >
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>Yes</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({ visibleModal: null })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>Cancel</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
+            </Modal>
+        )
+    }
+
+    reviewModal() {
+        return (
+            <Modal isVisible={this.state.visibleModal === 4}>
+                <View style={styles.modalContent}>
+                    <Text>Review Survey</Text>
+                    <Text>{`Question 1: ${this.state.surveyA1.question}`}</Text>
+                    <Text style={{marginBottom:5}}>{`Your answer: ${this.state.surveyA1.choice}`}</Text>                    
+                    <Text>{`Question 2: ${this.state.surveyA2.question}`}</Text>
+                    <Text>{`Your answer: ${this.state.surveyA2.choice}`}</Text>
+
+                    <TouchableOpacity onPress={()=>{                        
+                        this.postingData()
+                    }}
+                    >
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>Confirm</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => {
+                        this.setState({ visibleModal: null })
+                    }}>
+                        <View style={styles.modalButton}>
+                            <Text style={styles.modalText}>Cancel</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                </View>
+            </Modal>
+        )
+    }
 
 
 
     render() {
 
-        if (this.props.isSignedIn) {
+        if (this.props.isSignedIn && !this.state.isSent) {
 
             if (this.state.isTimeArray) {
 
                 return (
-                    <View style={styles.container}>
-                        <ScrollView>
+                    <ScrollView>
 
-                            {this.printRow()}
+                        {this.printRow()}
+                        {this.qOneModal()}
+                        {this.qTwoModal()}
+                        {this.sendingModal()}
+                        {this.reviewModal()}
 
-                        </ScrollView>
-
-                    </View>
+                    </ScrollView>
                 )
             }
 
             return (
                 <View>
                     <Text>Loading</Text>
+                </View>
+
+
+            )
+        }
+
+        else if (this.state.isSent) {
+
+            return (
+                <View style={styles.container}>
+                    <Text>Thank you for your contribution!</Text>
                 </View>
             )
         }
@@ -171,7 +469,6 @@ class Schedule extends React.Component {
             <View style={styles.container}>
                 <Text>Please sign in to begin</Text>
             </View>
-
 
         )
 
@@ -187,5 +484,25 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white'
+    },
+    modalContent: {
+        backgroundColor: 'white',
+        padding: 22,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    modalButton: {
+        backgroundColor: 'black',
+        padding: 12,
+        margin: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    modalText: {
+        color: "white"
     }
 })
